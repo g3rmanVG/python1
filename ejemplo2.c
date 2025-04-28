8de718d74499f7ca9cb46159609870cdde00e2e1
g3rmanVG <157979664+g3rmanVG@users.noreply.github.com>
2025-04-28 11:03:15 -0600

    Create ejemplo2.c

---
+++ ejemplo2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+  pid_t pid = fork();
+  if (pid == 0) {
+    printf("Proceso hijo (PID: %d) ejecutándose.\n", getpid());
+    return 42;
+  } else {
+    int status;
+    wait(&status);
+    printf("Proceso padre: hijo terminó con código %d\n", WEXITSTATUS(status));
+  }
+  return 0;
+}
